c4347b450a738360e607988c04f5b10d5d86c305
irvanln <irvina22@gmail.com>
2021-02-08 18:58:40 +0700

    controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index(){
+       //echo "ini test controller";
+       return view('welcome');
+    }
+}
